beda054f3ac0c9195e87fb739101dc794af2169b
KevinJonesM <alejones308@gmail.com>
2025-02-14 11:26:54 -0500

    New Commit

--- components/Todo.js
+++ components/Todo.js
@@ -1,50 +1,50 @@
 export class Todo {
-  constructor(data, selector) {
-      if (!data || !data.name) {
-          throw new Error();
-      }
+    constructor(data, selector, onComplete, onDelete) {
+        this._data = data;
+        this._selector = selector;
+        this._onComplete = onComplete;
+        this._onDelete = onDelete;
+    }
 
-      this._data = data;
-      this._selector = selector;
-  }
+    _setEventListeners(todoElement) {
+        const deleteButton = todoElement.querySelector(".todo__delete-btn");
+        const checkbox = todoElement.querySelector(".todo__completed");
 
-  _setEventListeners(todoElement) {
-      const deleteButton = todoElement.querySelector(".todo__delete-btn");
-      const checkbox = todoElement.querySelector(".todo__completed");
+        deleteButton.addEventListener("click", () => {
+            this._onDelete(); // Update the counter before removing
+            todoElement.remove();
+        });
 
-      deleteButton.addEventListener("click", () => {
-          todoElement.remove();
-      });
+        checkbox.addEventListener("change", () => {
+            this._data.completed = checkbox.checked;
+            this._onComplete(this._data.completed); // Update counter on check/uncheck
+        });
+    }
 
-      checkbox.addEventListener("change", () => {
-          this._data.completed = checkbox.checked;
-      });
-  }
+    _formatDate(date) {
+        if (!date) return "";
+        const dueDate = new Date(date);
+        return isNaN(dueDate) ? "" : `Due: ${dueDate.toLocaleDateString()}`;
+    }
 
-  _formatDate(date) {
-      if (!date) return "";
-      const dueDate = new Date(date);
-      return isNaN(dueDate) ? "" : `Due: ${dueDate.toLocaleDateString()}`;
-  }
+    getView() {
+        const template = document.querySelector(this._selector).content.cloneNode(true);
+        const todoElement = template.querySelector(".todo");
 
-  getView() {
-      const template = document.querySelector(this._selector).content.cloneNode(true);
-      const todoElement = template.querySelector(".todo");
+        const todoNameEl = todoElement.querySelector(".todo__name");
+        const todoCheckboxEl = todoElement.querySelector(".todo__completed");
+        const todoLabel = todoElement.querySelector(".todo__label");
+        const todoDateEl = todoElement.querySelector(".todo__date");
 
-      const todoNameEl = todoElement.querySelector(".todo__name");
-      const todoCheckboxEl = todoElement.querySelector(".todo__completed");
-      const todoLabel = todoElement.querySelector(".todo__label");
-      const todoDateEl = todoElement.querySelector(".todo__date");
+        todoNameEl.textContent = this._data.name;
+        todoCheckboxEl.checked = this._data.completed;
 
-      todoNameEl.textContent = this._data.name;
-      todoCheckboxEl.checked = this._data.completed;
+        const todoId = `todo-${this._data.id}`;
+        todoCheckboxEl.id = todoId;
+        todoLabel.setAttribute("for", todoId);
+        todoDateEl.textContent = this._formatDate(this._data.date);
 
-      const todoId = `todo-${this._data.id}`;
-      todoCheckboxEl.id = todoId;
-      todoLabel.setAttribute("for", todoId);
-      todoDateEl.textContent = this._formatDate(this._data.date);
-
-      this._setEventListeners(todoElement);
-      return todoElement;
-  }
+        this._setEventListeners(todoElement);
+        return todoElement;
+    }
 }
